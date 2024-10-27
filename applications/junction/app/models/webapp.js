97e5894ff86892063a6a0a1544553d3c99235fd0
cityowl / moti <cityowl@hey.com>
2024-10-27 17:34:07 +0530

    initial code for billing integration

--- applications/junction/app/models/webapp.js
+++ applications/junction/app/models/webapp.js
@@ -1,1 +1,5 @@
-import Model, { attr } from '@ember-data/model'; export default class WebappModel extends Model { @attr     slug; @attr modules; }
+import Model, { attr } from '@ember-data/model';
+export default class WebappModel extends Model {
+  @attr slug;
+  @attr modules;
+}
