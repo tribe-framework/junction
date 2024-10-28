fea869090f09e83e3ae7680c2c8f3358f99738af
cityowl / moti <cityowl@hey.com>
2024-10-28 18:25:54 +0530

    version validated

--- applications/junction/sync-types.php
+++ applications/junction/sync-types.php
@@ -7,18 +7,17 @@ if (($_ENV['TRIBE_API_URL'] ?? false) && $_ENV['TRIBE_API_URL'] != '') {
 }
 
 // re-create models folder
-$commands = "[[ app/models ]] && rm -r app/models && mkdir app/models; ";
+$commands = "[ -d app/models ] && rm -r app/models && mkdir app/models; ";
 // remove test for models
-$commands .= "[[ -d tests/unit/models ]] && rm -r tests/unit/models; ";
+$commands .= "[ -d tests/unit/models ] && rm -r tests/unit/models; ";
 
 foreach (array_keys($types) as $type) {
     if ($type == 'webapp' || in_array($type, ($types['webapp']['interface_urls'][basename(dirname(__FILE__))]['types'] ?? array_keys($types)))) {
 
         $type_hyphen = str_replace('_', '-', $type);
         $type_ucwords = str_replace(' ', '', ucwords(str_replace('_', ' ', $type)));
-        $commands .= "echo \"import Model, { attr } from '@ember-data/model'; export default class ".$type_ucwords."Model extends Model { @attr     slug; @attr modules; }\" > app/models/".$type_hyphen.".js; ";
+        $commands .= "echo \"import Model, { attr } from '@ember-data/model';\n\nexport default class ".$type_ucwords."Model extends Model {\n  @attr slug;\n  @attr modules;\n}\" > app/models/".$type_hyphen.".js; ";
     }
 }
 
 exec($commands);
-?>
