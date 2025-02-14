e0b190f640469dfec31721e8ac9e6bea68bd8374
cityowl / moti <cityowl@hey.com>
2025-02-14 09:25:23 +0530

    blueprint handling

--- applications/junction/app/components/api/auth-reference.js
+++ applications/junction/app/components/api/auth-reference.js
@@ -1,3 +1,12 @@
 import Component from '@glimmer/component';
+import hljs from 'highlight.js';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
-export default class ApiAuthReference extends Component {}
+export default class ApiAuthReference extends Component {
+  @action
+  highlightAll() {
+    hljs.highlightAll();
+  }
+}
