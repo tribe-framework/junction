5980f60c4b5a98439750b09be1fc5b675ff42df0
cityowl / moti <cityowl@hey.com>
2025-05-18 00:42:36 +0530

    bearer api access

--- applications/junction/app/adapters/application.js
+++ applications/junction/app/adapters/application.js
@@ -5,9 +5,19 @@ import { underscore } from '@ember/string';
 export default class ApplicationAdapter extends JSONAPIAdapter {
   host = ENV.TribeENV.API_URL;
   namespace = 'api.php';
-  headers = {
-    Authorization: `Bearer ${ENV.TribeENV.API_KEY}`,
-  };
+
+  get headers() {
+    let headers = {};
+
+    // API key here will show up as HTTP_X_API_KEY in PHP
+    if (ENV.TribeENV.API_KEY) {
+      headers['X-API-KEY'] = `Bearer ${ENV.TribeENV.API_KEY}`;
+    } else {
+      headers['X-API-KEY'] = `Bearer junction`;
+    }
+
+    return headers;
+  }
 
   pathForType(type) {
     return underscore(type);
